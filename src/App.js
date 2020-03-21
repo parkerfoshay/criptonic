2f6ca86f0b9dbe51d731eeb368dc4125b43f5fa0
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2020-03-21 13:23:29 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    console.log('Hi jeff and Willie')
+    
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world</h1>
+      <h1>{Math.floor(Math.random() * 100)}</h1>
     </div>
   );
 }
