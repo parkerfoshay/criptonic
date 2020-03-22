64148ec1090c6e17189b538b0454a2e5bfcedf73
parkerfoshay <40288355+parkerfoshay@users.noreply.github.com>
2020-03-22 13:47:00 -0700

    put together quick home screen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, {useEffect} from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./Containers/Home";
 
-function App() {
-
-  useEffect(() => {
-    console.log('Hi jeff and Willie')
-    
-  }, [])
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-      <h1>{Math.floor(Math.random() * 100)}</h1>
-    </div>
+    <Router>
+      <div>
+        <Route path="/" component={Home} />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/Containers/Home.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react'
+import SignIn from '../Components/SignInSide'
+
+export const Home = () => {
+    return (
+        <Fragment>
+           <SignIn /> 
+        </Fragment>
+    )
+}
